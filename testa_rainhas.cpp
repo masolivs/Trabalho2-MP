c0fc2fc3f44a60d9a8ff91ac25829606c22f731f
masolivs <marianasoli621@gmail.com>
2024-05-14 01:49:12 -0300

    Teste 29

--- testa_rainhas.cpp
+++ testa_rainhas.cpp
@@ -100,4 +100,8 @@ TEST_CASE( "Soluções Válidas - Não é solução do problema") {
         int result = solve8QueensFromFile("tabuleiros/board28.txt");
         REQUIRE(result == 0);
     }
+    SECTION( "Teste 29") {
+        int result = solve8QueensFromFile("tabuleiros/board29.txt");
+        REQUIRE(result == 0);
+    }
 }
